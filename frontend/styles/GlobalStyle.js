06692b684708ce729621668e7944604274d7e9d1
aijuridica <aijuridicabr@gmail.com>
2025-06-09 00:16:29 -0300

    Update GlobalStyle.js

--- frontend/styles/GlobalStyle.js
+++ frontend/styles/GlobalStyle.js
@@ -1,13 +1,1 @@
-
-import { createGlobalStyle } from 'styled-components';
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    font-family: 'Open Sans', sans-serif;
-    background-color: #F5F5F5;
-    color: #212529;
-  }
-`;
-
-export default GlobalStyle;
+import { createGlobalStyle } from "styled-components"; export const GlobalStyle = createGlobalStyle`body { font-family: Arial; }`;
