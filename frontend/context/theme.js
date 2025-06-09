26ebf3a0ab5c314c84ed44dd15eda6391417a86c
aijuridica <aijuridicabr@gmail.com>
2025-06-08 23:05:27 -0300

    Create theme.js

---
+++ frontend/context/theme.js
@@ -0,0 +1,14 @@
+
+export const theme = {
+  colors: {
+    primary: '#002C5F',
+    secondary: '#F5F5F5',
+    success: '#28A745',
+    danger: '#DC3545',
+    text: '#212529',
+  },
+  fonts: {
+    primary: '"Merriweather", serif',
+    secondary: '"Open Sans", sans-serif',
+  },
+};
