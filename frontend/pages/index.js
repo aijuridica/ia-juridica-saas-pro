90011409e332adf62e4b0f18b909d6d540067405
aijuridica <aijuridicabr@gmail.com>
2025-06-09 00:15:51 -0300

    Update index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,14 +1,1 @@
-
-import ButtonPrimary from '../components/ButtonPrimary'
-import InputField from '../components/InputField'
-
-export default function Home() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h1>IA Jurídica SaaS PRO</h1>
-      <InputField placeholder="Digite seu email" />
-      <InputField type="password" placeholder="Digite sua senha" />
-      <ButtonPrimary>Entrar</ButtonPrimary>
-    </div>
-  )
-}
+export default function Home() { return <h1>IA Jurídica SaaS PRO</h1> }
