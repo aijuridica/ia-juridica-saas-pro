bb0e515980dd9a619802bba8eaf038e66bf6e260
aijuridica <aijuridicabr@gmail.com>
2025-06-09 00:58:35 -0300

    Update GlobalStyle.js

--- frontend/styles/GlobalStyle.js
+++ frontend/styles/GlobalStyle.js
@@ -1,1 +1,13 @@
-// Global style code with design system
+
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: Arial, sans-serif;
+    background-color: #f8f9fa;
+    color: #212529;
+  }
+`;
+
+export default GlobalStyle;
