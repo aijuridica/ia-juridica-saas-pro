2c21658ec427e3a3954e0c7ff84d8e5c11626840
aijuridica <aijuridicabr@gmail.com>
2025-06-09 14:35:01 -0300

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from config import settings
 
-engine = create_engine(settings.SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+DATABASE_URL = "postgresql://usuario:senha@host:porta/nome_do_banco"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
